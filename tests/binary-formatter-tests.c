34efded28fb1ba52a8a536e7cfbee8f7cf2eebc7
Christian Hergert <chris@dronelabs.com>
2009-06-06 03:36:21 -0700

    Add a couple serializer unit tests.

--- tests/binary-formatter-tests.c
+++ tests/binary-formatter-tests.c
@@ -6,6 +6,32 @@ test1 (void)
 	g_assert (catalina_binary_formatter_new ());
 }
 
+static void
+test2 (void)
+{
+	gchar *b = NULL;
+	gsize l = 0;
+	GValue v = {G_TYPE_INT,};
+	g_value_set_int (&v, 42);
+	CatalinaFormatter *f = catalina_binary_formatter_new ();
+	g_assert (catalina_formatter_serialize (f, &v, &b, &l, NULL));
+}
+
+static void
+test3 (void)
+{
+	gchar *b = NULL;
+	gsize l = 0;
+	GValue v = {0,}, v2 = {0,};
+	g_value_init (&v, G_TYPE_INT);
+	g_value_set_int (&v, 42);
+	CatalinaFormatter *f = catalina_binary_formatter_new ();
+	g_assert (catalina_formatter_serialize (f, &v, &b, &l, NULL));
+	g_assert (b != NULL && l > 0);
+	g_assert (catalina_formatter_deserialize (f, &v2, b, l, NULL));
+	g_assert_cmpint (g_value_get_int (&v),==,g_value_get_int(&v2));
+}
+
 gint
 main (gint   argc,
       gchar *argv[])
@@ -14,6 +40,8 @@ main (gint   argc,
 	g_test_init (&argc, &argv, NULL);
 
 	g_test_add_func ("/CatalinaBinaryFormatter/new(1)", test1);
+	g_test_add_func ("/CatalinaBinaryFormatter/serialize<int>(1)", test2);
+	g_test_add_func ("/CatalinaBinaryFormatter/deserialize<int>(1)", test3);
 
 	return g_test_run ();
 }
