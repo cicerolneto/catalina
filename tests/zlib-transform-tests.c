8bf011ea2688aca2788737edfb53e5524e98c15a
Christian Hergert <chris@dronelabs.com>
2009-06-12 13:17:29 -0700

    Add a test that uses compression and serialization.

--- tests/zlib-transform-tests.c
+++ tests/zlib-transform-tests.c
@@ -42,6 +42,24 @@ test3 (void)
 	g_assert_cmpstr (outbuf,==,LONG_TEST_DATA);
 }
 
+static void
+test4 (void)
+{
+	GValue v1 = {0,}, v2 = {0,};
+	GError *error = NULL;
+	CatalinaStorage *storage = catalina_storage_new ();
+	g_assert (catalina_storage_open (storage, ".", "storage-tests.db", NULL));
+	g_object_set (storage, "formatter", catalina_binary_formatter_new (), NULL);
+	g_object_set (storage, "transform", catalina_zlib_transform_new (), NULL);
+	g_value_init (&v1, G_TYPE_STRING);
+	g_value_set_string (&v1, "my test string");
+	if (!catalina_storage_set_value (storage, "v-key", -1, &v1, &error))
+		g_error ("%s", error->message);
+	if (!catalina_storage_get_value (storage, "v-key", -1, &v2, &error))
+		g_error ("%s", error->message);
+	g_assert_cmpstr (g_value_get_string(&v1),==,g_value_get_string(&v2));
+}
+
 gint
 main (gint   argc,
       gchar *argv[])
@@ -53,6 +71,7 @@ main (gint   argc,
 	g_test_add_func ("/CatalinaZlibTransform/new(1)", test1);
 	g_test_add_func ("/CatalinaZlibTransform/read-write(1)", test2);
 	g_test_add_func ("/CatalinaZlibTransform/read-write(2)", test3);
+	g_test_add_func ("/CatalinaZlibTransform/read-write(3)", test4);
 
 	return g_test_run ();
 }
